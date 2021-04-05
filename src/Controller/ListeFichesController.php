431a260c486065cd1cdf26b5206fb495d90d7bb6
Rai0nZ <theoacod@gmail.com>
2021-04-05 18:07:59 +0200

    Creation liste Fiches

---
+++ src/Controller/ListeFichesController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Fiches;
+use App\Repository\FichesRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ListeFichesController extends AbstractController
+{
+    /**
+     * @Route("/liste_fiches", name="liste_fiches")
+     */
+    public function listeFiches(): Response {
+        $repository = $this->getDoctrine()->getRepository(Fiches::class);
+        $fiches = $repository->findAll();
+
+        return $this->render('liste_fiches/index.html.twig', [
+            'fiches' => $fiches
+        ]);
+    }
+}
