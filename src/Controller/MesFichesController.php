65ab9fd56ea9922b0249abd2d8fbf30eb8f2011d
bourdi <guillaume.bourdinaud@gmail.com>
2021-04-07 15:55:40 +0200

    le modifier/supprimer

--- src/Controller/MesFichesController.php
+++ src/Controller/MesFichesController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Fiches;
+use App\Form\CreerFicheType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -23,7 +25,36 @@ class MesFichesController extends AbstractController
             'fiches' => $fiches
         ]);    
     }
+    /**
+     * @Route("/{id}/edit", name="modifier_fiche", methods={"GET","POST"})
+     */
+    public function modifier_fiche(Request $request,Fiches $fiche): Response
+    {
+        $form = $this->createForm(CreerFicheType::class, $fiche);
+        $form->handleRequest($request);
 
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
 
-    
+            return $this->redirectToRoute('mes_fiches');
+        }
+
+        return $this->render('modifier-fiche.html.twig', [
+            'fiches' => $fiche,
+            'form' => $form->createView(),
+        ]);
+    }
+        /**
+     * @Route("/{id}", name="supprimer_fiche", methods={"DELETE"})
+     */
+    public function delete(Request $request, Fiches $fiche): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$fiche->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($fiche);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('mes-fiches');
+    }
 }
--- src/Form/CreerFicheType.php
+++ src/Form/CreerFicheType.php
@@ -2,9 +2,11 @@
 
 namespace App\Form;
 
+use App\Entity\Disciplines;
 use App\Entity\Fiches;
+use App\Entity\Niveaux;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
