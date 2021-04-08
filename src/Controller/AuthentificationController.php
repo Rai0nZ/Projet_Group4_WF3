f1596d2b7d868b573b222e37159fe2de2db56341
sabrinaTht <sabrina.tahanout@yahoo.fr>
2021-04-08 15:38:36 +0200

    travail sur controller mes-fiches

--- src/Controller/AuthentificationController.php
+++ src/Controller/AuthentificationController.php
@@ -33,9 +33,9 @@ class AuthentificationController extends AbstractController
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
+        $lastUsername = $authenticationUtils->getLastUsername();      
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->redirectToRoute('homepage');
     }
 
     /**
